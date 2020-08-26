5668d6666239a91e009f0f33ef9fcdbde279730c
Diego Valerio <67572551+DiegoJVD@users.noreply.github.com>
2020-08-25 21:41:26 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("\nWhat is your name? ");
-var name = Console.ReadLine();
-var date = DateTime.Now;
-Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
-Console.Write("\nPress any key to exit...");
-Console.ReadKey(true);
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
